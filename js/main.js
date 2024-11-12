b37118250d2986bfc35bdb7c731bd669670f6c35
Max Bahar <max.a.bahar@gmail.com>
2024-11-11 22:56:45 -0500

    Added cleaned block groups file, template for the D3 project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Read in data from the GeoJSON file
+d3.json('data/block_groups.geojson').then(data => {
+    
+    // Log data for confirmation
+    console.log(data);
+
+    // TO DO: Visualizations go here
+
+});
