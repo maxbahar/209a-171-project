53cc2a5123a054fdb72b5736aecc6067057a2965
Max Bahar <max.a.bahar@gmail.com>
2024-11-29 17:09:07 -0500

    Latest Milestone 4 updates

--- js/main.js
+++ js/main.js
@@ -16,12 +16,12 @@ let variableMap = {
     "age_75_84" : "Age 75-84", "age_85over" : "Age 85+",
     "gender_m" : "Male", "gender_f" : "Female", "gender_unknown" : "Unknown", 
     "party_npp" : "Nonpartisan", "party_dem" : "Democrat", "party_rep" : "Republican", "party_lib" : "Libertarian", 
-    "party_grn" : "Green", "party_con" : "Conservative", "party_ain" : "American Independent", "party_scl" : "Socialist", "party_oth" : "Unknown", 
+    "party_grn" : "Green", "party_con" : "Conservative", "party_ain" : "Amer. Ind.", "party_scl" : "Socialist", "party_oth" : "Unknown", 
     "eth1_eur" : "European", "eth1_hisp" : "Hispanic",
     "eth1_aa" : "African American", "eth1_esa" : "East/South Asian", "eth1_oth" : "Other", "eth1_unk" : "Unknown",
     "lang_english" : "English", "lang_spanish" : "Spanish", "lang_portuguese" : "Portuguese", "lang_chinese" : "Chinese", "lang_italian" : "Italian",
     "lang_vietnamese" : "Vietnamese", "lang_other" : "Other", "lang_unknown" : "Unknown", "mean_hh_income" : "Mean Household Income",
-    "ALAND20" : "Land Area", "2020_turnout_pct" : "Voter Turnout Percentage", "2020_absent_pct" : "Voter Absence Percentage", "2020_reg" : "Voters Registered",
+    "ALAND20" : "Land Area", "2020_turnout_pct" : "Voter Turnout Percentage", "2020_absent_pct" : "Voter Absence Percentage", "2020_registered" : "Voters Registered",
     "2020_turnout" : "Voter Turnout", "2020_turnout_pct_pred" : "Predicted Voter Turnout Percentage", "2020_absent" : "Voter Absence",
     "2020_absent_pct_pred" : "Predicted Voter Absence Percentage", "2020_turnout_pred" : "Predicted Voter Turnout", "2020_absent_pred" : "Predicted Voter Absence"
 }
@@ -74,7 +74,7 @@ function createVis(data) {
     let stateVotes = 0, stateReg = 0;
     geoData["county"].features.forEach(d => {
         stateVotes += d.properties["2020_turnout"];
-        stateReg += d.properties["2020_reg"];
+        stateReg += d.properties["2020_registered"];
     });
     userGuess = stateVotes / stateReg;
     document.getElementById("stateAvg").innerText = userGuess.toLocaleString();
